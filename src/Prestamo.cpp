045e472aed203b3429280d8676abac69ba198a5d
Aurelio Cordoba V <acvalerio25@gmail.com>
2024-02-12 00:48:39 -0600

    Aca se suben los archivos de la clase prestamo, ahora imprime la tabla con los valores
    Signed-off-by: Aurelio Cordoba V <acvalerio25@gmail.com>

--- src/Prestamo.cpp
+++ src/Prestamo.cpp
@@ -1,6 +1,7 @@
 #include "Prestamo.hpp"
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 
 Prestamo::Prestamo(Tipo tipo, double tasaInteresAnual, int cantidadCuotas, double montoPrestamo)
     : tipoPrestamo(tipo), tasaInteresAnual(tasaInteresAnual), cantidadCuotas(cantidadCuotas), montoPrestamo(montoPrestamo) {}
@@ -10,8 +11,9 @@ double Prestamo::calcularCuotaMensual() {
     double cuotaMensual = (montoPrestamo * tasaInteresMensual) / (1 - pow(1 + tasaInteresMensual, -cantidadCuotas));
     return cuotaMensual;
 }
+
 int main() {
-    // Pedir al usuario los datos del préstamo
+    // Se pide al usuario los datos del préstamo
     int tipo;
     std::cout << "Tipo de prestamo (0: Personal, 1: Hipotecario, 2: Prendario): ";
     std::cin >> tipo;
@@ -28,7 +30,7 @@ int main() {
     std::cout << "Monto del prestamo: ";
     std::cin >> montoPrestamo;
 
-    // Crear objeto Prestamo con los datos ingresados por el usuario
+    // Se crea el objeto Prestamo con los datos ingresados por el usuario
     Prestamo::Tipo tipoPrestamo;
     if (tipo == 0) {
         tipoPrestamo = Prestamo::PERSONAL;
@@ -43,9 +45,20 @@ int main() {
 
     Prestamo prestamo(tipoPrestamo, tasaInteresAnual, cantidadCuotas, montoPrestamo);
 
-    // Calcular la cuota mensual y mostrarla
-    double cuotaMensual = prestamo.calcularCuotaMensual();
-    std::cout << "La cuota mensual del prestamo es: $" << cuotaMensual << std::endl;
+    // Se imprime la tabla 
+    std::cout << std::setw(8) << "Mes" << std::setw(15) << "Cuota Mensual" << std::setw(15) << "Intereses" << std::setw(15) << "Deuda" << std::setw(20) << "Montante Restante\n";
+    std::cout << std::string(70, '-') << std::endl;
+
+    // Se calculan e imprimen los valores de las cuotas en pantalla
+    double deuda = montoPrestamo;
+    for (int i = 1; i <= cantidadCuotas; ++i) {
+        double cuotaMensual = prestamo.calcularCuotaMensual();
+        double intereses = deuda * (tasaInteresAnual / 12 / 100);
+        double amortizacion = cuotaMensual - intereses;
+        deuda -= amortizacion;
+
+        std::cout << std::setw(8) << i << std::fixed << std::setprecision(2) << std::setw(15) << cuotaMensual <<  std::setw(15) << intereses <<  std::setw(15) << amortizacion  << std::setw(20) << deuda << "\n";
+    }
 
     return 0;
 }
